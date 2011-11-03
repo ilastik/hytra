f87ec7d86fc7860fa8a7c20ac4120aecfea2de51
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2011-11-03 18:13:22 +0100

    write projected tifs directly

--- segmentation_ilastik/vistracking.py
+++ segmentation_ilastik/vistracking.py
@@ -2,12 +2,14 @@
 
 import sys
 import os.path as path
+import vigra
 import numpy as np
 import h5py
 from empryonic.io import LineageH5
 
 def relabel( volume, replace ):
     mp = np.arange(0,np.amax(volume)+1, dtype=volume.dtype)
+    mp[1:] = 255
     mp[replace.keys()] = replace.values()
     return mp[volume]
 
@@ -22,12 +24,12 @@ label2color = []
 print "-- mapping to colors"
 # init first timestep with random colors
 label2color.append({})
-with LineageH5(fns[0], 'r') as f:
-    print path.basename(fns[0])
-    labels = f['objects/meta/id'].value
-    for label in labels:
-        color = np.random.randint(1,255)
-        label2color[-1][label] = color
+#with LineageH5(fns[0], 'r') as f:
+#    print path.basename(fns[0])
+#    labels = f['objects/meta/id'].value
+#    for label in labels:
+#        color = np.random.randint(2,255)
+#        label2color[-1][label] = color
 
 # map the rest
 for fn in fns[1:]:
@@ -43,9 +45,13 @@ for fn in fns[1:]:
             label2color[-1][app[0]] = np.random.randint(1,255)
 
         for move in f.get_moves():
+            if not label2color[-2].has_key(move[0]):
+                label2color[-2][move[0]] = np.random.randint(1,255)
             label2color[-1][move[1]] = label2color[-2][move[0]]
 
         for division in f.get_divisions():
+            if not label2color[-2].has_key(division[0]):
+                label2color[-2][division[0]] = np.random.randint(1,255)
             ancestor_color = label2color[-2][division[0]]
             label2color[-1][division[1]] = ancestor_color
             label2color[-1][division[2]] = ancestor_color
@@ -57,6 +63,8 @@ print "-- creating colored arrays"
 for idx, fn in enumerate(fns):
     with h5py.File(fn, 'r') as f:
         print path.basename(fn)
-        relabeled = relabel(f['segmentation/labels'].value, label2color[idx])
-    with h5py.File("vis_"+ path.basename(fn), 'w') as f_out:
-        f_out.create_dataset('connected_components', data=relabeled, dtype=np.uint8, compression=2)
+        proj = f['segmentation/labels'].value.max(axis=2)
+        relabeled = relabel(proj, label2color[idx])
+    vigra.impex.writeImage(np.asarray(relabeled,dtype=np.uint8), "vis_"+ path.basename(fn)+".tif")
+    #with h5py.File("vis_"+ path.basename(fn), 'w') as f_out:
+    #    f_out.create_dataset('connected_components', data=relabeled, dtype=np.uint8, compression=2)
