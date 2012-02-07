4a9d49c3e0824f68b63806565862d56381a7644c
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2012-02-07 20:41:11 +0100

    specified a ilp class

--- tracking/include/reasoning/kanade_reasoner.h
+++ tracking/include/reasoning/kanade_reasoner.h
@@ -1,11 +1,23 @@
 #ifndef KANADE_REASONER_H
 #define KANADE_REASONER_H
 
+#include "ilp_construction.h"
 #include "reasoning/reasoner.h"
 
 namespace Tracking {
 class Traxel;
 class HypothesesGraph;
+
+class KanadeIlp : public IntegerLinearProgram {
+ public:
+  KanadeIlp& add_init_hypothesis(double cost); // initialization
+  KanadeIlp& add_term_hypothesis(double cost); // termination
+  KanadeIlp& add_trans_hypothesis(double cost); // translation
+  KanadeIlp& add_div_hypothesis(double cost); // division
+  KanadeIlp& add_fp_hypothesis(double cost); // false positive
+};
+
+
 
 class Kanade : public Reasoner {
     public:
@@ -15,8 +27,10 @@ class Kanade : public Reasoner {
 
     private:
     // copy and assingment have to be implemented, yet
-    //SingleTimestepTraxelMrf(const SingleTimestepTraxelMrf&) {};
-    //SingleTimestepTraxelMrf& operator=(const SingleTimestepTraxelMrf&) { return *this;};
+    Kanade(const Kanade&) {};
+    Kanade& operator=(const Kanade&) { return *this;};
+
+    KanadeIlp ilp_;
 };
 
 } /* namespace Tracking */
--- tracking/src/kanade_reasoner.cpp
+++ tracking/src/kanade_reasoner.cpp
@@ -4,6 +4,39 @@ using namespace std;
 
 namespace Tracking {
 
+////
+//// class KanadeIlp
+////
+  KanadeIlp& KanadeIlp::add_init_hypothesis(double cost) {
+    return *this;
+  }
+
+
+
+  KanadeIlp& KanadeIlp::add_term_hypothesis(double cost) {
+    return *this;
+  }
+
+
+
+  KanadeIlp& KanadeIlp::add_trans_hypothesis(double cost) {
+    return *this;
+  }
+
+
+
+  KanadeIlp& KanadeIlp::add_div_hypothesis(double cost) {
+    return *this;
+  }
+
+
+
+  KanadeIlp& KanadeIlp::add_fp_hypothesis(double cost) {
+    return *this;
+  }
+
+
+
 void Kanade::formulate( const HypothesesGraph& hypotheses ) {
 }
 
