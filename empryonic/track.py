e4ddf75f1f555fe1b12efc35e580150b37c854c2
mschiegg <martin.schiegg@iwr.uni-heidelberg.de>
2013-02-23 17:28:10 +0100

    track.py: replaced ctracking by pgmlink

--- empryonic/track.py
+++ empryonic/track.py
@@ -9,27 +9,27 @@ if(os.path.exists(module_path_in_source) or os.path.exists('../' + module_path_i
    sys.path.append(module_path_in_source)
    sys.path.append('../' + module_path_in_source)
 try:
-   import ctracking as _ct
+   import pgmlink as _ct
 
-   from ctracking import FieldOfView
+   from pgmlink import FieldOfView
 
-   from ctracking import ComLocator
-   from ctracking import IntmaxposLocator
-   from ctracking import Traxel as cTraxel
-   from ctracking import Traxels as cTraxels
-   from ctracking import TraxelStore
+   from pgmlink import ComLocator
+   from pgmlink import IntmaxposLocator
+   from pgmlink import Traxel as cTraxel
+   from pgmlink import Traxels as cTraxels
+   from pgmlink import TraxelStore
 
-   from ctracking import Event
-   from ctracking import EventType
-   from ctracking import EventVector
-   #from ctracking import FixedCostTracking
-   #from ctracking import ShortestDistanceTracking
-   #from ctracking import CellnessTracking
-   #from ctracking import BotTracking
-   from ctracking import MrfTracking
-   #from ctracking import KanadeTracking   
+   from pgmlink import Event
+   from pgmlink import EventType
+   from pgmlink import EventVector
+   #from pgmlink import FixedCostTracking
+   #from pgmlink import ShortestDistanceTracking
+   #from pgmlink import CellnessTracking
+   #from pgmlink import BotTracking
+   from pgmlink import ChaingraphTracking
+   #from pgmlink import KanadeTracking   
    
 except ImportError as error:
    if VERBOSE:
-      print "-! ctracking module not found or incompatible. Related functionality not available.: %s" % error 
+      print "-! pgmlink module not found or incompatible. Related functionality not available.: %s" % error 
       
