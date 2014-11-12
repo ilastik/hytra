1cdf0f313da98709f1ea981d5c3e4077654ce43a
Steffen_Wolf <Steffen.Wolf@stud.uni-heidelberg.de>
2014-11-12 11:05:33 +0100

    merge with master

--- toolbox/h5_to_gt.py
+++ toolbox/h5_to_gt.py
@@ -25,14 +25,13 @@ def generate_groundtruth(options):
 
         for t in xrange(options.start,options.end+1,1):
 
-            #just for fun :)
+            # #just for fun :)
             sys.stdout.write('\r')
             # the exact output you're looking for:
             sys.stdout.write("[%-20s] %d%%" % ('='*(20*t/options.end), (100*t/options.end)))
             sys.stdout.flush()
 
-            print options.end - t,
-            outputFileName = options.output_dir.rstrip('/')+ "/%04d.h5" % t
+            outputFileName = options.output_dir.rstrip('/')+ "/%03d.h5" % t
             #create output file
             if os.path.exists(outputFileName):
                 os.remove(outputFileName)
@@ -99,40 +98,89 @@ def generate_groundtruth(options):
 
                     inId_to_outId_funct[t] = np.vectorize(inId_to_outId_dics[t].get)
 
+                    applist = []
+                    disapplist = []
+
+
                     if(len(merlist) > 0):
                         #TODO: THIS IS WRONG NEED TO ADD NUMBER OF MERGERS AND OUT ID
                         trackingdata.create_dataset("Mergers", data=np.asarray(merlist), dtype='u2') 
 
                     if(len(inId_to_outId_funct) > 1):
-                        if("Moves" in inputfile["tracking"]["{0:04d}".format(t)].keys()):
-                            moves = np.array(inputfile["tracking"]["{0:04d}".format(t)]["Moves"]).squeeze().astype(np.int)
+                        if("Moves" in inputfile["tracking"]["{0:03d}".format(t)].keys()):
+                            moves = np.array(inputfile["tracking"]["{0:03d}".format(t)]["Moves"]).squeeze().astype(np.int)
                             moves = np.reshape(moves, (-1,2))
+                            print inId_to_outId_dics , moves[:,0],inId_to_outId_funct[t-1](moves[:,0])
                             moves[:,0] = inId_to_outId_funct[t-1](moves[:,0])
                             moves[:,1] = inId_to_outId_funct[t](moves[:,1])
+
+                            #make moves without match to disapp and app
+                            movelist = moves.tolist()
+                            for mov in movelist:
+                                if (-1 in mov):
+                                    print "\nresolving ", mov, "at ",t
+                                    if(mov[0] == -1 and mov[1] > 0):#appearance
+                                        applist.append(mov[1])
+                                    if(mov[1] == -1 and mov[0] > 0):#disappearance
+                                        print mov[0]
+                                        disapplist.append(mov[0])
+
+                                    movelist = [mov for mov in movelist if (not -1 in mov)]
+
+                                    moves = np.array(movelist)
+
+                            if(np.any(moves == -1)):
+                                print "-1 in moves !!! at timestep",t
+                                print moves
+                                print "#####################################"
+                                print np.array(inputfile["tracking"]["{0:03d}".format(t)]["Moves"]).squeeze().astype(np.int)
+                                exit()
+
+
                             trackingdata.create_dataset("Moves", data=moves, dtype='u2')
                             movedict[t] = moves
 
-                        if("Splits" in inputfile["tracking"]["{0:04d}".format(t)].keys()):
-                            splits = np.array(inputfile["tracking"]["{0:04d}".format(t)]["Splits"]).squeeze().astype(np.int)
+                        if("Splits" in inputfile["tracking"]["{0:03d}".format(t)].keys()):
+                            splits = np.array(inputfile["tracking"]["{0:03d}".format(t)]["Splits"]).squeeze().astype(np.int)
                             splits = np.reshape(splits,(-1,3))
                             splits[:,0] = inId_to_outId_funct[t-1](splits[:,0])
                             splits[:,1:] = inId_to_outId_funct[t](splits[:,1:])
+
+                            # splitlist = splits.tolist()
+                            # for spl in splits:
+                            #     if (-1 in spl):
+                            #         print "error can not resolve split"
+                            #         exit()
+
+
+                            if(np.any(splits == 0)):
+                                print "0 in moves !!! at timestep",t
+                                print splits
+                                exit()
+
+                            if(np.any(splits == -1)):
+                                print "-1 in moves !!! at timestep",t
+                                print splits
+                                exit()
+
+
                             trackingdata.create_dataset("Splits", data=splits, dtype='u2') 
                             splitdict[t] = splits
 
-                        applist = []
                         for i in inId_to_outId_dics[t]:
                             outid = inId_to_outId_dics[t][i]
-                            if((t in movedict and not np.any(movedict[t][:,1]==outid)) and 
-                               (t in splitdict and not np.any(splitdict[t][:,1:]==outid))):
-                                applist.append(outid)
+                            if outid > 0:
+                                if((t in movedict and not np.any(movedict[t][:,1]==outid)) and 
+                                   (t in splitdict and not np.any(splitdict[t][:,1:]==outid))):
+                                    applist.append(outid)
 
-                        disapplist = []
+
                         for i in inId_to_outId_dics[t-1]:
                             outid = inId_to_outId_dics[t-1][i]
-                            if((t in movedict and not np.any(movedict[t][:,0]==outid)) and 
-                               (t in splitdict and not np.any(splitdict[t][:,0]==outid))):
-                                disapplist.append(outid)
+                            if outid > 0:
+                                if((t in movedict and not np.any(movedict[t][:,0]==outid)) and 
+                                   (t in splitdict and not np.any(splitdict[t][:,0]==outid))):
+                                    disapplist.append(outid)
 
 
                         if(len(applist) > 0):
@@ -144,61 +192,6 @@ def generate_groundtruth(options):
 
 
 
-# raw_image = np.array(raw_h5['/'.join(options.raw_path.split('/'))][timestep, ..., 0]).squeeze().astype(np.float32)
-
-def unusedCode():
-
-    if not options.output_file:
-        options.output_file = options.input_dir + '/.h5'
-
-
-    if os.path.exists(options.output_file):
-        os.remove(options.output_file)
-
-    f = h5py.File(options.output_file, 'r')
-
-    with h5py.File(options.output_file, 'w') as out_h5:
-
-        ids = out_h5.create_group('ids')
-        tracking = out_h5.create_group('tracking')
-
-
-
-        # object ids per frame
-        objects_per_frame = []
-        for frame in range(label_volume.shape[2]):
-            objects = np.unique(label_volume[..., frame, 0])
-            ids.create_dataset(format(frame, "04"), data=objects, dtype='u2')
-            objects_per_frame.append(set(objects))
-
-        # move, mitosis and split events
-        tracking_frame = tracking.create_group(format(0, "04"))
-        for frame in range(1, label_volume.shape[2]):
-            tracking_frame = tracking.create_group(format(frame, "04"))
-
-            # intersect track id sets of both frames, and place moves in HDF5 file
-            tracks_in_both_frames = objects_per_frame[frame - 1] & objects_per_frame[frame] - set([0])
-            moves = np.array([list(tracks_in_both_frames), list(tracks_in_both_frames)]).transpose()
-            tracking_frame.create_dataset("Moves", data=moves, dtype='u2')
-
-            # add the found splits as both, mitosis and split events
-            if frame in split_events.keys():
-                splits_in_frame = split_events[frame]
-                mitosis = splits_in_frame.keys()
-                if len(mitosis) > 0:
-                    tracking_frame.create_dataset("Mitosis", data=np.array(mitosis), dtype='u2')
-                splits = [[key] + value for key, value in splits_in_frame.iteritems()]
-		# make sure all splits have the same dimension
-		max_split_length = max(map(len, splits))
-		min_split_length = min(map(len, splits))
-		if min_split_length != max_split_length:
-			print("In timestep {}: Found splits longer than minimum {}, cutting off children to make number equal!".format(frame, min_split_length))
-			for i, split in enumerate(splits):
-				splits[i] = split[0:min_split_length]
-
-
-                if len(splits) > 0:
-                    tracking_frame.create_dataset("Splits", data=np.array(splits), dtype='u2')
 
 
 
