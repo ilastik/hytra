966403adfd89bc4873b38e177e71abb48af4cd80
Carsten Haubold <carstenhaubold@googlemail.com>
2015-04-07 10:26:59 +0200

    Plot score vs precision and other measures

--- cvpr15_eval/plot_score_vs_precision.py
+++ cvpr15_eval/plot_score_vs_precision.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../toolbox/.')
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
-
+import math
 import structsvm
 import trackingfeatures
 
@@ -41,20 +41,58 @@ if __name__ == "__main__":
 
     # compute scores
     scores = []
+    num_divs = []
+    num_tracks = []
+    lengths = []
     for lt in lineage_trees:
         feat_vec = np.expand_dims(lt.get_expanded_feature_vector([-1, 2]), axis=1)
         structsvm.utils.apply_feature_normalization(feat_vec, means, variances)
         score = np.dot(weights, feat_vec[:, 0])
         scores.append(score)
+        num_divs.append(len(lt.divisions))
+        num_tracks.append(len(lt.tracks))
+        lengths.append(sum([t.length for t in lt.tracks]))
 
-    # print scores
-    # print losses
-
-    # plot
+    # scatter plot
     plt.figure()
     plt.hold(True)
     plt.scatter(precisions, scores)
     plt.xlabel("Precision")
     plt.ylabel("Score")
-
     plt.savefig(options.out_file)
+
+    # sort according to precision and plot again
+    log_scores = map(math.log, scores)
+    prec_score_pairs = zip(list(precisions), scores, num_divs, num_tracks, lengths)
+    prec_score_pairs.sort(key=lambda x: x[1])
+
+    plt.figure()
+    plt.hold(True)
+    plt.plot(zip(*prec_score_pairs)[1], zip(*prec_score_pairs)[0])
+    plt.ylabel("Precision")
+    plt.xlabel("Score")
+    filename, extension = os.path.splitext(options.out_file)
+    plt.savefig(filename + "_sorted" + extension)
+
+    plt.figure()
+    plt.hold(True)
+    plt.scatter(zip(*prec_score_pairs)[2], zip(*prec_score_pairs)[1], c='b', label='Num divisions')
+    plt.scatter(zip(*prec_score_pairs)[3], zip(*prec_score_pairs)[1], c='r', label='Num tracks')
+    # plt.plot(zip(*prec_score_pairs)[4], zip(*prec_score_pairs)[1], c='g', label='overall lengths')
+    plt.xlabel("Length")
+    plt.ylabel("Score")
+    plt.legend()
+    filename, extension = os.path.splitext(options.out_file)
+    plt.savefig(filename + "_length_score" + extension)
+
+    plt.figure()
+    plt.hold(True)
+    plt.scatter(zip(*prec_score_pairs)[2], zip(*prec_score_pairs)[0], c='b', label='Num divisions')
+    plt.scatter(zip(*prec_score_pairs)[3], zip(*prec_score_pairs)[0], c='r', label='Num tracks')
+    # plt.scatter(zip(*prec_score_pairs)[4], zip(*prec_score_pairs)[0], c='g', label='overall lengths')
+    plt.xlabel("Length")
+    plt.ylabel("Precision")
+    plt.legend()
+    filename, extension = os.path.splitext(options.out_file)
+    plt.savefig(filename + "_length_precision" + extension)
+    
