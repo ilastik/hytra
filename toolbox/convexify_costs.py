8bc26a74afbc86f9b6b467c1a467f0809c697b78
Carsten Haubold <carstenhaubold@googlemail.com>
2016-01-11 10:35:29 +0100

    Fix convexification so that tracking runs at last!

--- toolbox/convexify_costs.py
+++ toolbox/convexify_costs.py
@@ -11,6 +11,11 @@ from progressbar import ProgressBar
 def listify(l):
     return [[e] for e in l]
 
+def check(feats):
+    grad = feats[1:] - feats[0:-1]
+    for i in range(len(grad) - 1):
+        assert(grad[i+1] > grad[i])
+
 def convexify(l, eps):
     features = np.array(l)
     if features.shape[1] != 1:
@@ -24,18 +29,23 @@ def convexify(l, eps):
         previousGradient = 0
         while pos >= 0 and pos < features.shape[0]:
             newGradient = features[pos] - features[pos-direction]
-            if newGradient == previousGradient:
-                # cost function's derivative is constant, add epsilon
+            if np.abs(newGradient - previousGradient) < eps:
+                # cost function's derivative is roughly constant, add epsilon
                 previousGradient += eps
                 features[pos] = features[pos-direction] + previousGradient
             elif newGradient < previousGradient:
                 # cost function got too flat, set feature value to match old slope
+                previousGradient += eps
                 features[pos] = features[pos-direction] + previousGradient
             else:
                 # all good, continue with new slope
                 previousGradient = newGradient
                 
             pos += direction
+    try:
+        check(features)
+    except:
+        print("Failed convexifying to {}".format(features))
     return listify(features.flatten())
 
 if __name__ == "__main__":
@@ -61,7 +71,11 @@ if __name__ == "__main__":
     for seg in segmentationHypotheses:
         for f in ['features', 'appearanceFeatures', 'disappearanceFeatures']:
             if f in seg:
-                seg[f] = convexify(seg[f], args.epsilon)
+                try:
+                    seg[f] = convexify(seg[f], args.epsilon)
+                except:
+                    print("Convexification failed for feature {} of :{}".format(f, seg))
+                    exit(0)
         # division features are always convex (is just a line)
         progressBar.show()
 
