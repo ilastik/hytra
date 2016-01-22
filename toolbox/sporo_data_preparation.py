f3eee469ab521ca61b8cf83ee6211a3d05bbd49b
Carsten Haubold <carstenhaubold@googlemail.com>
2016-01-22 12:37:14 +0100

    fix indentation

--- toolbox/sporo_data_preparation.py
+++ toolbox/sporo_data_preparation.py
@@ -3,7 +3,7 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Take two tiff files, one for the sporozite channel and one for the nucleus channel, \
-    		and create two files needed for further processing: a 3-channel hdf5 volume and a 1-channel nucleus HDF5.',
+            and create two files needed for further processing: a 3-channel hdf5 volume and a 1-channel nucleus HDF5.',
                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--sporo', required=True, type=str, dest='sporoFilename',
                         help='Filename of the sporozyte tiff')
@@ -14,10 +14,10 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
 
-	sporoChannel = vigra.impex.readVolume(args.sporoFilename)
-	nucleusChannel = vigra.impex.readVolume(args.nucleusFilename)
-	resultVolume = np.zeros((nucleusChannel.shape[0], nucleusChannel.shape[1], nucleusChannel.shape[2], 3), dtype='float32')
-	resultVolume[...,1] = sporoChannel[...,0]
-	resultVolume[...,2] = nucleusChannel[...,0]
-	vigra.impex.writeHDF5(resultVolume, args.threeChannelOut, 'exported_data')
-	vigra.impex.writeHDF5(nucleusChannel, args.nucleusChannelOut, 'exported_data')
+    sporoChannel = vigra.impex.readVolume(args.sporoFilename)
+    nucleusChannel = vigra.impex.readVolume(args.nucleusFilename)
+    resultVolume = np.zeros((nucleusChannel.shape[0], nucleusChannel.shape[1], nucleusChannel.shape[2], 3), dtype='float32')
+    resultVolume[...,1] = sporoChannel[...,0]
+    resultVolume[...,2] = nucleusChannel[...,0]
+    vigra.impex.writeHDF5(resultVolume, args.threeChannelOut, 'exported_data')
+    vigra.impex.writeHDF5(nucleusChannel, args.nucleusChannelOut, 'exported_data')
