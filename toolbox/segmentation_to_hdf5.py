4e0b9a20dbf9d44d83f6e34a704f4c6a6e2a1e17
Carsten Haubold <carstenhaubold@googlemail.com>
2016-04-30 10:12:23 +0200

    Make segmentation_to_hdf5 script output datasets of the proper shape. WARNING: still contains "swapaxis" to make the rest of the pipeline work for me.

--- toolbox/segmentation_to_hdf5.py
+++ toolbox/segmentation_to_hdf5.py
@@ -4,10 +4,27 @@ import vigra
 import argparse
 
 def segmentation_to_hdf5(options):
+    """
+    The generated segmentation is one HDF5 dataset per timestep,
+    and each of these datasets has shape 1(t),x,y,z,1(c).
+    """
     out_h5 = h5py.File(options.hdf5Path, 'w')
     for timeframe in range(len(options.tif_input_files)):
         data = vigra.impex.readImage(options.tif_input_files[timeframe], dtype='UINT16') # sure UINT32?
-        internalPath = options.hdf5ImagePath % (timeframe, timeframe + 1, data.shape[0], data.shape[1], data.shape[2])
+        
+        if timeframe == 0:
+            print("Found image of shape {}".format(data.shape))
+        
+        # put dimension in front for the time step
+        data = np.expand_dims(data, axis=0)
+        while len(data.shape) < 5:
+            data = np.expand_dims(data, axis=-1)
+        data = np.swapaxes(data, 1, 2)
+
+        if timeframe == 0:
+            print("Changed into shape {}".format(data.shape))
+
+        internalPath = options.hdf5ImagePath % (timeframe, timeframe + 1, data.shape[1], data.shape[2], data.shape[3])
         out_h5.create_dataset(internalPath, data=data, dtype='u2', compression='gzip')
 
 
