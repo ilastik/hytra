6e30223c888edfe8f380be0f33e9b6173837559e
Carsten Haubold <carstenhaubold@googlemail.com>
2016-06-08 17:59:56 +0200

    Add setup.py that installs the toolbox module (without scripts)

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="trackingIAL",
+    version="0.1.0",
+    author="Carsten Haubold",
+    author_email="carsten.haubold@iwr.uni-heidelberg.com",
+    description=("Tracking pipeline of the IAL lab at the University of Heidelberg "),
+    license="BSD",
+    keywords="cell tracking divisions ilastik",
+    url="http://github.com/ilastik/trackingIAL",
+    packages=['toolbox', 'toolbox.core', 'toolbox.util', 'toolbox.pluginsystem', 'toolbox.plugins', 'toolbox.dvid'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7"
+    ],
+)
