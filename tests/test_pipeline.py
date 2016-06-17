d207b476b908b7de1f56accf91f7569a411db7d1
Carsten Haubold <carstenhaubold@googlemail.com>
2016-06-17 15:53:31 +0200

    Add small division test dataset and add a pipeline test with divisions

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -26,3 +26,23 @@ def test_mergerResolvingTestDataset():
 
     with open('../tests/mergerResolvingTestDataset/ctc_RES/res_track.txt', 'r') as resultFile:
         assert(resultFile.read() == '1 0 3 0\n2 0 3 0\n')
+
+def test_divisionTestDataset():
+    check_call(["python",
+                "../hytra/configtemplates/create_config.py",
+                "--in",
+                "../tests/divisionTestDataset/config_template.ini",
+                "--out",
+                "../tests/divisionTestDataset/test_config.ini",
+                "embryonicDir",
+                ".."
+                ])
+    check_call(["python", "pipeline.py", "--config", "../tests/divisionTestDataset/test_config.ini"])
+
+    expectedIds = [[0, 1], [0, 1], [0, 2, 3], [0, 2, 3]]
+    for f in range(4):
+        frame = vigra.impex.readImage('../tests/divisionTestDataset/ctc_RES/mask00{}.tif'.format(f))
+        assert(np.all(np.unique(frame) == expectedIds[f]))
+
+    with open('../tests/divisionTestDataset/ctc_RES/res_track.txt', 'r') as resultFile:
+        assert(resultFile.read() == '1 0 1 0\n2 2 3 1\n3 2 3 1\n')
