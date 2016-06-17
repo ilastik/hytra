b446d69f70e1a2568411934cd63628354d679534
Carsten Haubold <carstenhaubold@googlemail.com>
2016-06-17 09:41:16 +0200

    Add test for axesconversion.getFrameSlicing

--- tests/util/test_axesconversion.py
+++ tests/util/test_axesconversion.py
@@ -2,7 +2,7 @@ import numpy as np
 import hytra.util.axesconversion as ax
 
 def test_conversion_default():
-    a = np.zeros([123,64])
+    a = np.zeros([123, 64])
     b = ax.adjustOrder(a, 'yx')
     assert(b.ndim == 5)
     assert(b.shape[0] == 1)
@@ -12,7 +12,7 @@ def test_conversion_default():
     assert(b.shape[4] == 1)
 
 def test_conversion_other():
-    a = np.zeros([123,64, 987])
+    a = np.zeros([123, 64, 987])
     b = ax.adjustOrder(a, 'yxc', 'cxtzy')
     assert(b.ndim == 5)
     assert(b.shape[0] == 987)
@@ -20,3 +20,15 @@ def test_conversion_other():
     assert(b.shape[2] == 1)
     assert(b.shape[3] == 1)
     assert(b.shape[4] == 123)
+
+def test_frame_slicing():
+    a = np.zeros([123, 64, 987])
+    b = a[ax.getFrameSlicing('xyt', 12)]
+    assert(b.ndim == 2)
+    assert(b.shape[0] == 123)
+    assert(b.shape[1] == 64)
+
+    b = a[ax.getFrameSlicing('xyt', 12, 'y')]
+    assert(b.ndim == 2)
+    assert(b.shape[0] == 123)
+    assert(b.shape[1] == 987)
