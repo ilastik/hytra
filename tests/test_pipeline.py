5da92361d7c8df832e794894583f6c263c6161b5
Carsten Haubold <carstenhaubold@googlemail.com>
2016-07-20 11:19:32 +0200

    Add a test for the pipeline without scripts

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -89,3 +89,25 @@ def test_divisionTestDataset_withoutTracklets():
 
     with open('../tests/divisionTestDataset/ctc_RES/res_track.txt', 'r') as resultFile:
         assert(resultFile.read() == '1 0 1 0\n2 2 3 1\n3 2 3 1\n')
+
+def test_noscripts_mergerResolvingTestDataset():
+    check_call(["python",
+                "../hytra/configtemplates/create_config.py",
+                "--in",
+                "../tests/mergerResolvingTestDataset/config_template.ini",
+                "--out",
+                "../tests/mergerResolvingTestDataset/test_config.ini",
+                "embryonicDir",
+                ".."
+                ])
+    check_call(["python", "pipelineNoScripts.py", "--config", "../tests/mergerResolvingTestDataset/test_config.ini"])
+
+    for f in range(4):
+        frame = vigra.impex.readImage('../tests/mergerResolvingTestDataset/ctc_RES/mask00{}.tif'.format(f))
+        assert(np.all(np.unique(frame) == [0,1,2]))
+
+    with open('../tests/mergerResolvingTestDataset/ctc_RES/res_track.txt', 'r') as resultFile:
+        assert(resultFile.read() == '1 0 3 0\n2 0 3 0\n')
+
+if __name__ == "__main__":
+    test_noscripts_mergerResolvingTestDataset()
