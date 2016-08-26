fd42ffb60d15ec50e3dfd11f461e076fc5a1011d
Carsten Haubold <carstenhaubold@googlemail.com>
2016-08-26 10:07:09 +0200

    JST: code cleanup, use helper class instead of tuple

--- hytra/jst/classifiertrainingexampleextractor.py
+++ hytra/jst/classifiertrainingexampleextractor.py
@@ -3,8 +3,9 @@ Provide methods to find positive and negative training examples from a hypothese
 a ground truth mapping, in the presence of multiple competing segmentation hypotheseses.
 '''
 
-import numpy as np
 import logging
+import numpy as np
+import attr
 from hytra.core.random_forest_classifier import RandomForestClassifier
 
 def getLogger():
@@ -19,12 +20,17 @@ def trainDetectionClassifier(hypothesesGraph, gtFrameIdToGlobalIdsWithScoresMap,
     Positive examples are those with the highest jaccard score, while negative examples can either 
     just not be the best match for a GT label, or also be not matched at all.
 
-
-
     **Returns**: a trained random forest
     """
     # create a list of all elements, sort them by their jaccard score, then pick from both ends?
     getLogger().debug("Extracting candidates")
+
+    # create helper class for candidates, and store a list of these
+    @attr.s
+    class Candidate(object):
+        ''' Helper class to combine a hytpotheses graph `node` and its `score` to find the proper samples for classifier training '''
+        node = attr.ib()
+        score = attr.ib(validator=attr.validators.instance_of(float))
     candidates = []
 
     nodeTraxelMap = hypothesesGraph.getNodeTraxelMap()
@@ -33,10 +39,10 @@ def trainDetectionClassifier(hypothesesGraph, gtFrameIdToGlobalIdsWithScoresMap,
             globalIdsAndScores = nodeTraxelMap[node].Features['JaccardScores']
             globalIdsAndScores = sorted(globalIdsAndScores, key=lambda x: x[1])
             bestScore = globalIdsAndScores[-1][1]
-            candidates.append( (node, bestScore) )
+            candidates.append(Candidate(node, bestScore))
     
     assert(len(candidates) >= numSamples)
-    candidates.sort(key=lambda x: x[1])
+    candidates.sort(key=lambda x: x.score)
 
     # pick the first and last numSamples/2, and extract their features?
     # use RandomForestClassifier's method "extractFeatureVector"
@@ -46,10 +52,10 @@ def trainDetectionClassifier(hypothesesGraph, gtFrameIdToGlobalIdsWithScoresMap,
     # TODO: make sure that the positive examples were all selected in the GT mapping
 
     getLogger().debug("construct feature matrix")
-    node = selectedSamples[0][0]
+    node = selectedSamples[0].node
     if selectedFeatures is None:
         selectedFeatures = nodeTraxelMap[node].Features.keys()
-        forbidden = ['JaccardScores', 'id', 'filename' , 'Polygon', 'detProb', 'divProb', 'com']
+        forbidden = ['JaccardScores', 'id', 'filename', 'Polygon', 'detProb', 'divProb', 'com']
         forbidden += [f for f in selectedFeatures if f.count('_') > 0]
         for f in forbidden:
             if f in selectedFeatures:
@@ -58,10 +64,10 @@ def trainDetectionClassifier(hypothesesGraph, gtFrameIdToGlobalIdsWithScoresMap,
 
     rf = RandomForestClassifier(selectedFeatures=selectedFeatures)
     features = rf.extractFeatureVector(nodeTraxelMap[node].Features, singleObject=True)
-    featureMatrix = np.zeros( [len(selectedSamples), features.shape[1]] )
+    featureMatrix = np.zeros([len(selectedSamples), features.shape[1]])
     featureMatrix[0, :] = features
-    for idx, nodeAndScore in enumerate(selectedSamples[1:]):
-        features = rf.extractFeatureVector(nodeTraxelMap[nodeAndScore[0]].Features, singleObject=True)
+    for idx, candidate in enumerate(selectedSamples[1:]):
+        features = rf.extractFeatureVector(nodeTraxelMap[candidate.node].Features, singleObject=True)
         featureMatrix[idx + 1, :] = features
 
     rf.train(featureMatrix, labels)
