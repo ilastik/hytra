ece3b41b6f846a865c4095824a1754d347e9830c
Letitia Parcalabescu <leti_parcalabescu@yahoo.com>
2016-11-28 17:31:15 +0100

    Write out frames containing no detections too

--- hytra/core/mergerresolver.py
+++ hytra/core/mergerresolver.py
@@ -359,7 +359,9 @@ class MergerResolver(object):
         """
 
         traxelIdPerTimestepToUniqueIdMap, uuidToTraxelMap = hytra.core.jsongraph.getMappingsBetweenUUIDsAndTraxels(self.model)
-        timesteps = [t for t in traxelIdPerTimestepToUniqueIdMap.keys()]
+        # timesteps = [t for t in traxelIdPerTimestepToUniqueIdMap.keys()]
+        # there might be empty frames. We want them as output too.
+        timesteps = [str(t).decode("utf-8") for t in range(int(min(traxelIdPerTimestepToUniqueIdMap.keys())) , int(max(traxelIdPerTimestepToUniqueIdMap.keys()))+1 )]
 
         mergers, detections, links, divisions = hytra.core.jsongraph.getMergersDetectionsLinksDivisions(self.result, uuidToTraxelMap)
 
