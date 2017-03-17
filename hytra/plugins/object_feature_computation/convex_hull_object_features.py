209027c82fc706cccaf0b6a6c59060ebf2f98563
Carsten Haubold <carstenhaubold@googlemail.com>
2017-03-17 11:57:05 +0100

    Convex Hull features: adopt new vigra function name

--- hytra/plugins/object_feature_computation/convex_hull_object_features.py
+++ hytra/plugins/object_feature_computation/convex_hull_object_features.py
@@ -11,8 +11,8 @@ class ConvexHullObjectFeatures(object_feature_computation_plugin.ObjectFeatureCo
     omittedFeatures = ['Polygon']
 
     def computeFeatures(self, rawImage, labelImage, frameNumber, rawFilename):
-        featureDict =  vigra.analysis.extractConvexHullFeatures(labelImage.squeeze().astype(np.uint32),
-                                                                ignoreLabel=0)
+        featureDict =  vigra.analysis.extract2DConvexHullFeatures(labelImage.squeeze().astype(np.uint32),
+                                                                  ignoreLabel=0)
         if 'Center' in featureDict:
             # old vigra versions simply call that feature "Center" which conflicts with other features 
             featureDict['Hull Center'] = featureDict['Center']
