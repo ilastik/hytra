fa52e12b29f18d85fa241d66be6358cf1c1b5658
k-dominik <k-dominik@users.noreply.github.com>
2019-02-26 15:54:46 +0100

    ref to ilastik, version bump

--- setup.py
+++ setup.py
@@ -4,20 +4,21 @@ from distutils.core import setup
 
 setup(
     name="HyTra",
-    version="0.1.0",
+    version="0.1.1",
     author="Carsten Haubold",
-    author_email="carsten.haubold@iwr.uni-heidelberg.com",
+    author_email="team@ilastik.org",
     description=("Tracking pipeline of the IAL lab at the University of Heidelberg"),
     license="BSD",
     keywords="cell tracking divisions ilastik",
-    url="http://github.com/chaubold/hytra",
-    packages=['hytra', 'hytra.core', 'hytra.util', 'hytra.pluginsystem', 'hytra.dvid'],
-    package_data={'hytra': ['plugins/*/*']},
-    include_package_data = True,
+    url="http://github.com/ilastik/hytra",
+    packages=["hytra", "hytra.core", "hytra.util", "hytra.pluginsystem", "hytra.dvid"],
+    package_data={"hytra": ["plugins/*/*"]},
+    include_package_data=True,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 2.7"
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
